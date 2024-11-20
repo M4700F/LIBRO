01aa4fdb5684a2b58e70e595a9efc79bdc60c237
Maroof Ahmed <maroofahmed@iut-dhaka.edu>
2024-11-20 18:28:10 +0600

    Updated

--- src/jframe/LoginPage.java
+++ src/jframe/LoginPage.java
@@ -103,6 +103,7 @@ public class LoginPage extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
+        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
